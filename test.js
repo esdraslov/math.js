09cd4760ca2fd7ec633349caa9415e703b732633
esdraslov <edvan.marcelino56@gmail.com>
2023-11-03 01:11:41 -0300

    second test way, no 'require()' method

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-let { pi, pow } = require("index.js")
+let pow = (n, x) => {
+	let sum = 0
+	for (let i=0;i < x;i++) {
+		sum += n * n
+	}
+	return sum
+}
+
+console.log(`googol is 10^100, googol = ${pow(10, 100)}`)
